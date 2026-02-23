eca6a393e7b687e14a54b2ae8caa0756e60bf3af
Meet <meet@sciative.com>
2026-02-23 18:21:11 +0530

    chore(release): update module pointers

--- start-dev.sh
+++ start-dev.sh
@@ -9,9 +9,12 @@ echo "🔄 Updating backend submodules..."
 git submodule update --init --recursive --remote
 
 echo "📝 Saving new release state to Git..."
-
-git add .
-git commit -m "chore(release): update module pointers"
-git push origin main
+if ! git diff-index --quiet HEAD; then
+    git add .
+    git commit -m "chore(release): update module pointers"
+    git push origin main
+else
+    echo "⚡ No Git changes needed in shell."
+fi
 
 echo "✅ Server shell updated in CodeCommit."
